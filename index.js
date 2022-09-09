71daf1784b7a85bbff60b16b68e492276a68deff
Cole A. Kendall <kendallcole7@gmail.com>
2022-09-08 22:26:49 -0400

    add prototyping for routing login traffic

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var mysql = require('mysql');
 var express = require('express');
 var app = express();
+const cors = require('cors');
 
 var con;
 var Host = "";
 var User = "";
 var Password = "";
 var Database = "";
+
+app.use(cors());
+app.use(express.json());
 
 process.argv.forEach(function (val, index, array) {
 
@@ -31,16 +35,23 @@ process.argv.forEach(function (val, index, array) {
         database: Database
       });
 
+    
+
 });
 
-app.get('/login', function(req, err) {
-    if (err) throw err;
-    console.log(req.body.userName);
-    console.log(req.body.password);
+con.query("CREATE TABLE IF NOT EXISTS users( userid int NOT NULL AUTO_INCREMENT, username VARCHAR(50) NOT NULL, password VARCHAR(256), PRIMARY KEY(userid) );");
+
+app.post('/login', function(req, res) {
+    con.query("SELECT * FROM users WHERE username = ? and password = ?", [req.body.userName, req.body.password], function(err, res, fields) {
+        if (res.length == 0) con.query("INSERT INTO users (username, password) VALUES ( ? , ? )", [req.body.userName, req.body.password], function(err, res){
+            if (err) return err;
+            console.log("Added user ( " + req.body.userName + " ) to table.");
+        });
+        else {
+            console.log("User Found ( "+ res[0].username +" )");
+        }
+    });
 });
 
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected");
-});
+app.listen(8081);
