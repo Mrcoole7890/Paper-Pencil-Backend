088cc4786d2dff02613953869216ee2c9cfefad4
Cole A. Kendall <kendallcole7@gmail.com>
2022-11-17 02:36:09 -0500

    magic happened here

--- index.js
+++ index.js
@@ -5,23 +5,6 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 
-let parseToken = function(req, res){
-    let tokenHeaderKey = process.env.JWT_HEADER_SEC_KEY;
-    let jwtSecretKey = process.env.JWT_SECRET_KEY;
-  
-    try {
-        const token = req.header(tokenHeaderKey);
-        console.log(token)
-        const verified = jwt.verify(token, jwtSecretKey);
-        if(verified){
-            return verified;
-        }else{
-            return {'status':403};
-        }
-    } catch (error) {
-        return {'status':403};
-    }
-}
 
 dotenv.config();
 app.use(cors());
@@ -66,17 +49,20 @@ con.query("CREATE TABLE IF NOT EXISTS member("
     + "PRIMARY KEY(memberid));");
 
 app.post('/login', function(req, responce) {
-    con.query("SELECT * FROM users WHERE username = ? and password = ?", [req.body.userName, req.body.password], function(err, res, fields) {
-        if (res.length == 0) {
+
+    getUser(req.body.userName).then((dbResults) => {
+        console.log(dbResults);
+        if (dbResults.length == 0) {
             if (err) return err;
             responce.send({status : "UNotF"})
             console.log("User Not Found!");
         }
         else {
-            responce.send({status : "Good", token: generateToken(JSON.stringify(res[0]))})
-            console.log("User Found ( "+ res[0].username +" )");
+            responce.send({status : "Good", token: generateToken(JSON.stringify(dbResults[0]))})
+            console.log("User Found ( "+ dbResults[0].userid +" )");
         }
     });
+
 });
 
 app.post('/register', function(req, responce) {
@@ -98,6 +84,28 @@ app.post("/user", (req, res) => {
     res.send(data);
 });
 
+app.get("/clan", (req,res) =>{
+    let data = parseToken(req,res);
+    console.log(data);
+});
+
+let parseToken = function(req, res){
+    let tokenHeaderKey = process.env.JWT_HEADER_SEC_KEY;
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  
+    try {
+        const token = req.header(tokenHeaderKey);
+        const verified = jwt.verify(token, jwtSecretKey);
+        if(verified){
+            return verified;
+        }else{
+            return {'status':403};
+        }
+    } catch (error) {
+        return {'status':403};
+    }
+}
+
 function generateToken(obj) {
     let jwtSecretKey = process.env.JWT_SECRET_KEY;
     const token = jwt.sign(obj, jwtSecretKey);
@@ -113,34 +121,85 @@ function generateMockData() {
 
 function addPlayer(username, password) {
     con.query("INSERT INTO users (username, password) VALUES ( ? , ? )", [username, password], function(err, res){
-        if (err) return console.log(err);
+        if (err) return err;
         console.log("Added user ( " + username + " ) to table.");
     });
 }
 
 function addBase(userid, size, location) {
     con.query("INSERT INTO base (playerid, size, location) VALUES ( ?, ?, ? )", [userid, size, location], function(err, res){
-        if (err) return console.log(err);
+        if (err) return err;
         console.log("Added base to table.");
     });
 }
 
 function addClan(userid, clanName) {
     con.query("INSERT INTO clan (playerid, clanname) VALUES ( ?, ? )", [userid, clanName], function(err, res){
-        if (err) return console.log(err);
+        if (err) return err;
         console.log("Added clan to table.");
     });
 }
 
 function addMember(userid, name, quality) {
     con.query("INSERT INTO member (playerid, membername, quality, health) VALUES ( ?, ?, ?, 100 )", [userid, name, quality], function(err, res){
-        if (err) return console.log(err);
+        if (err) return err;
         console.log("Added member to table.");
     });
 }
 
+function getClan(userid) {
+    return new Promise((resolve, reject) => {
+        var queryStr = 
+        "Select clanname " + 
+        "from clan " +
+        "Where playerid = ?";
+
+        var queryVar = [userid];
+
+        con.query(queryStr, queryVar, function(err, rows, fields){
+            if (err) reject(err);
+            else {
+                resolve(rows);
+            }
+        });
+    });
+}
+
+function getMembers(playerid) {
+    return new Promise((resolve, reject) => {
+        var queryStr = 
+        "Select membername " + 
+        "from member " +
+        "Where playerid = ?";
+
+        var queryVar = [playerid];
+
+        con.query(queryStr, queryVar, function(err, rows, fields){
+            if (err) reject(err);
+            else {
+                resolve(rows);
+            }
+        });
+    });  
+}
+
+function getUser(username) {
+    return new Promise((resolve, reject) => {
+        var queryStr = 
+        "Select userid, username " + 
+        "from users " +
+        "Where username = ?";
+
+        var queryVar = [username];
+
+        con.query(queryStr, queryVar, function(err, rows, fields){
+            if (err) reject(err);
+            else {
+                resolve(rows);
+            }
+        });
+    });  
+}
 
 generateMockData();
 app.listen(8081);
-
-
